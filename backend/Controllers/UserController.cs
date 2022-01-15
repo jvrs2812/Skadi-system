1afcd0f097a1f1677fc273089305b1ad4df2bdba
joao sousa <jvrs_souza@hotmail.com>
2022-01-15 15:42:34 -0300

    feat: Alter to repositories type consult

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -1,10 +1,13 @@
 
 using backend.db;
+using backend.exception;
 using backend.model;
+using backend.repository.user;
 using backend.requestmodels;
 using backend.response;
 using backend.settings;
 using backend.UseCases;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
@@ -13,10 +16,9 @@ namespace backend.Controllers
     public class UserController : ControllerBase
     {
         [HttpPost("api1/register")]
-        public async Task<IActionResult> PostRegister(
+        public IActionResult PostRegister(
             [FromBody] UserRequest userModelRequest,
-            [FromServices] SKADIDBContext context
-, DateTime dateTime)
+            [FromServices] SKADIDBContext context)
         {
             try
             {
@@ -28,80 +30,92 @@ namespace backend.Controllers
                     return BadRequest(new ResultResponse<UserRequest>(query.ToList()));
                 }
 
-                var emailIsNotValid = context.User.FirstOrDefault(x => x.Email == userModelRequest.Email) != null;
 
-                if (emailIsNotValid)
-                {
-                    return BadRequest(new ResultResponse<UserRequest>("Email já utilizado"));
-                }
+                var userCreate = new UserRepository(context: context, userModelRequest);
 
-                var CnpjIsNotValid = context.User.FirstOrDefault(x => x.CNPJ == userModelRequest.CNPJ) != null;
+                userCreate.ValidateUser();
 
-                if (CnpjIsNotValid)
-                {
-                    return BadRequest(new ResultResponse<UserRequest>("CNPJ já utilizado"));
-                }
+                userCreate.CreateUserFather();
 
-                var UserRegister = new User();
-
-                UserRegister.CreatedAt = DateTime.UtcNow;
-                UserRegister.Email = userModelRequest.Email;
-                UserRegister.CNPJ = userModelRequest.CNPJ;
-                UserRegister.Name = userModelRequest.Name;
-                UserRegister.Password = userModelRequest.Password;
-                UserRegister.Id = GetHashCode().ToString();
-                UserRegister.Role = "";
-
-                context.User.Add(UserRegister);
-                context.SaveChanges();
-
-                var UserResponse = new UserRegisterResponse();
-                var token = TokenService.GenerateToken(UserRegister);
-
-                UserResponse.Name = UserRegister.Name;
-                UserResponse.Id = UserRegister.Id;
-                UserResponse.Email = UserRegister.Email;
-                UserResponse.CNPJ = UserRegister.CNPJ;
-                UserResponse.Token = token;
-
-                return Created($"api1/login", new ResultResponse<UserRegisterResponse>(UserResponse));
+                return Created($"api1/login", "");
             }
-            catch (Exception err)
+            catch (ValidateException err)
+            {
+                return BadRequest(new ResultResponse<User>(err.Message));
+            }
+            catch
             {
                 return StatusCode(500, new ResultResponse<User>("Erro interno 31231x123 contate o desenvolvedor"));
             }
         }
+
         [HttpPost("api1/login")]
-        public async Task<IActionResult> PostLogin(
-            [FromBody] UserRequest user,
+        public IActionResult PostLogin(
+            [FromBody] UserLoginRequest user,
             [FromServices] SKADIDBContext context
         )
         {
+            if (!ModelState.IsValid)
+            {
+                var query = from state in ModelState.Values
+                            from error in state.Errors
+                            select error.ErrorMessage;
+                return BadRequest(new ResultResponse<UserRequest>(query.ToList()));
+            }
+            try
+            {
+                var UserLogin = new UserRepository(context, user);
 
+                var login = UserLogin.Login();
 
-            return Ok();
+                return Ok(new ResultResponse<UserLoginResponse>(login));
+            }
+            catch (ValidateException err)
+            {
+                return BadRequest(new ResultResponse<UserLoginResponse>(err.Message));
+            }
+            catch
+            {
+                return StatusCode(500, "Erro 82831x081 contate o desenvolvedor");
+            }
+
         }
-
+        [Authorize]
         [HttpGet("api1/user/{id}")]
-        public async Task<IActionResult> GetUser(
+        public IActionResult GetUser(
             [FromRoute] String id,
             [FromServices] SKADIDBContext context
         )
         {
+            if (!ModelState.IsValid)
+            {
+                var query = from state in ModelState.Values
+                            from error in state.Errors
+                            select error.ErrorMessage;
+                return BadRequest(new ResultResponse<UserGetResponse>(query.ToList()));
+            }
             try
             {
-                var user = context.User.FirstOrDefault(x => x.Id == id);
+                var user = context.User.FirstOrDefault(x => x.id == id);
 
                 if (user == null)
-                    return NotFound(new ResultResponse<User>("Usuário não encontrado."));
+                    return NotFound(new ResultResponse<UserGetResponse>("Usuário não encontrado."));
 
-                return Ok(new ResultResponse<User>(user));
+                var userResponse = new UserGetResponse();
+
+                userResponse.id = user.id;
+                userResponse.name = user.name;
+                userResponse.email = user.email;
+
+
+                return Ok(new ResultResponse<UserGetResponse>(userResponse));
             }
-            catch (Exception e)
+            catch
             {
-                return StatusCode(500, new ResultResponse<User>("Erro 50920x989 relatar ao desenvolvedor"));
+                return StatusCode(500, new ResultResponse<UserGetResponse>("Erro 50920x989 relatar ao desenvolvedor"));
             }
 
         }
+
     }
 }
